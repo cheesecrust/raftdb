061f962f744976d8b5b1c70cc6f2932949ad25eb
cheesecrust <jack0810@kookmin.ac.kr>
2024-09-04 13:46:23 +0900

    FEATURE: Socket HeartBeat

--- socket.c
+++ socket.c
@@ -11,13 +11,13 @@ void* run_socket(void* arg) {
         struct sockaddr_in addr;
         socklen_t len = sizeof(addr);
 
-        // printf("Node %d is listening for messages\n", node->node_id);
         recvfrom(node->socket_fd, (char*)buffer, 1024, MSG_WAITALL, (struct sockaddr*)&addr, &len);
         printf("Node %d received: %s\n", node->node_id, buffer);
 
         if (strncmp(buffer, "REQUEST_VOTE", 12) == 0) {
             int term, candidate_id;
             sscanf(buffer, "REQUEST_VOTE %d %d", &term, &candidate_id);
+
             if (term > node->current_term) {
                 node->current_term = term;
                 node->state = FOLLOWER;
@@ -27,8 +27,25 @@ void* run_socket(void* arg) {
                 node->voted_for = candidate_id;
                 sendto(node->socket_fd, "VOTE_GRANTED", 12, 0, (const struct sockaddr*)&addr, len);
             }
+        } else if (strncmp(buffer, "VOTE_GRANTED", 12) == 0) {
+            node->votes++;
+
+            if (node->votes > NUM_NODES / 2) {
+                node->state = LEADER;
+                printf("Node %d is now the leader\n", node->node_id);
+                for (int i = 1; i < NUM_NODES; i++) {
+                    sendto(node->socket_fd, "LEADER_ELECTED ", 14, 0, (const struct sockaddr*)&nodes[i], len);
+                }
+            }
         } else if (strncmp(buffer, "HEARTBEAT", 9) == 0) {
-            // TODO: implement
+            int term, leader_id;
+            sscanf(buffer, "HEARTBEAT %d %d", &term, &leader_id);
+            node->current_term = term;
+            node->leader_id = leader_id;
+            node->last_heartbeat = time(NULL);
+            node->voted_for = -1;
+            node->votes = 0;
+            node->state = FOLLOWER;
         }
     }
 }
